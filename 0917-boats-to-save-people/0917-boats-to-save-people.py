803998d7022fd4d0fc4b1927ba17be1514c0f6a8
Abiy Hailu <abiyhailu120@gmail.com>
2025-05-03 14:21:32 +0300

    Time: 47 ms (67.85%), Space: 23.3 MB (59.69%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.py
+++ 0917-boats-to-save-people/0917-boats-to-save-people.py
@@ -5,16 +5,9 @@ class Solution:
         left = 0
         right = len(people) - 1
         while left <= right:
-            if people[left] + people[right] > limit:
-                boats += 1
-                right -= 1
-            elif people[left] + people[right] == limit:
+            if people[left] + people[right] <= limit:
                 left += 1
-                right -= 1
-                boats += 1
-            else:
-                left += 1
-                right -= 1
-                boats += 1
+            right -= 1
+            boats += 1
         return boats
                 
